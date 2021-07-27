b9c5fe629e1cf883338abb62628dd79925b8c680
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-07-27 10:47:44 -0300

    Initial Commit

---
+++ CoreDataQuickStart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataQuickStart
+//
+//  Created by Aline Osana Escobar on 27/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
