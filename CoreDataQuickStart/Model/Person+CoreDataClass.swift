a0c7f009731508b481c2b47800a5efa57332a452
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-07-29 22:14:33 -0300

    initial setup with core data

---
+++ CoreDataQuickStart/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataQuickStart
+//
+//  Created by Aline Osana Escobar on 29/07/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
